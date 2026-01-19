579735d3591b82b3bf99c2315302bbd7f55dff52
muhammadalics10 <muhmmadali.cs10@gmail.com>
2026-01-19 16:10:32 +0500

    Empty

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
 }
 
 android {
