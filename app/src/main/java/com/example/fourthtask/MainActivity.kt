5981d423ab08c2c01eaeeec077134c7d111c9f8b
muhammadalics10 <muhmmadali.cs10@gmail.com>
2026-01-19 13:06:26 +0500

    Add network handling on main

--- app/src/main/java/com/example/fourthtask/MainActivity.kt
+++ app/src/main/java/com/example/fourthtask/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.fourthtask
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.fourthtask.data.api.RetrofitClient
+import com.example.fourthtask.data.db.AppDatabase
+import com.example.fourthtask.data.repository.PostRepository
+import com.example.fourthtask.ui.main.MainViewModel
+import com.example.fourthtask.utils.NetworkUtils
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val db = AppDatabase.getInstance(this)
+        val repo = PostRepository(RetrofitClient.api, db.postDao())
+        viewModel = MainViewModel(repo)
+
+        val isOnline = NetworkUtils.isInternetAvailable(this)
+
+        viewModel.loadPosts(isOnline)
+
+        viewModel.postsLiveData.observe(this) {
+            findViewById<TextView>(R.id.txtData).text = it.joinToString("\n") { post ->
+                post.title
+            }
+        }
+
+        if (!isOnline) {
+            findViewById<ImageView>(R.id.imgNoInternet).visibility = View.VISIBLE
+            findViewById<TextView>(R.id.txtMessage).visibility = View.VISIBLE
+        }
+
+        findViewById<Button>(R.id.btnCrash).setOnClickListener {
+            throw RuntimeException("Test Crash")
         }
     }
 }
